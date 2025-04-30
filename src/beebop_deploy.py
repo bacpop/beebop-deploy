de46d33e7db1d5a1d0cf78ddf23aef43179be4f3
David Mears <clarinet.david@gmail.com>
2025-04-30 14:44:16 +0100

    Update constellation version from 1.0.0. to 1.4.0

--- src/beebop_deploy.py
+++ src/beebop_deploy.py
@@ -115,7 +115,7 @@ class BeebopConfig:
 
 def beebop_constellation(cfg):
     # 1. redis
-    redis_mounts = [constellation.ConstellationMount("redis-volume", "/data")]
+    redis_mounts = [constellation.ConstellationVolumeMount("redis-volume", "/data")]
     redis = constellation.ConstellationContainer(
         "redis", cfg.redis_ref, configure=redis_configure, mounts=redis_mounts
     )
@@ -124,7 +124,7 @@ def beebop_constellation(cfg):
     api_env = {"REDIS_HOST": redis.name,
                "STORAGE_LOCATION": cfg.api_storage_location,
                "DBS_LOCATION": cfg.api_dbs_location}
-    api_mounts = [constellation.ConstellationMount("storage",
+    api_mounts = [constellation.ConstellationVolumeMount("storage",
                                                    "/beebop/storage")]
     api = constellation.ConstellationContainer(
         "api", cfg.api_ref, environment=api_env, mounts=api_mounts,
@@ -136,7 +136,7 @@ def beebop_constellation(cfg):
 
     # 4. worker
     worker_env = {"REDIS_HOST": redis.name}
-    worker_mounts = [constellation.ConstellationMount("storage",
+    worker_mounts = [constellation.ConstellationVolumeMount("storage",
                                                       "/beebop/storage")]
     # Constellation starts containers on no network, then joins them to
     # the network. So we get the container to sleep for 5s to allow for it
