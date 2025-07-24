3e69a3e17be5b2195791c1b9eeceb0cb7b6699e2
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-07-24 21:15:40 +0100

    Try to mock out vault call

--- test/test_cli.py
+++ test/test_cli.py
@@ -33,12 +33,14 @@ def test_cli_parse():
 
 
 def test_args_passed_to_start(monkeypatch):
-    monkeypatch.setenv("VAULT_AUTH_GITHUB_TOKEN", "abcde")
     with mock.patch('src.beebop_cli.beebop_start') as f:
-        beebop_cli.main(["start", "prod"])
+        with mock.patch('src.beebop_deploy.vault.resolve_secrets') as g:
+            beebop_cli.main(["start", "prod"])
 
     assert f.called
     assert f.call_args[0][1] == {"pull_images": False}
+
+    assert g.called
 
     with mock.patch('src.beebop_cli.beebop_start') as f:
         beebop_cli.main(["start", "prod", "--pull"])
