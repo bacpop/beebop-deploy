20469b312e92037613be25abb2fdc4a032fc9e69
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-07-24 21:01:06 +0100

    Add env var for vault key in testing

--- test/test_cli.py
+++ test/test_cli.py
@@ -32,8 +32,8 @@ def test_cli_parse():
     assert beebop_cli.parse(["upgrade"]) == ("config", None, "upgrade", {}, {})
 
 
-def test_args_passed_to_start():
-    os.environ["VAULT_GITHUB_AUTH_TOKEN"] = "abcde"
+def test_args_passed_to_start(monkeypatch):
+    monkeypatch.setenv("VAULT_GITHUB_AUTH_TOKEN", "abcde")
     with mock.patch('src.beebop_cli.beebop_start') as f:
         beebop_cli.main(["start", "prod"])
 
