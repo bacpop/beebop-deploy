fc063605fa32ed547e6ac88171ec213bfd7c717d
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-07-24 21:03:31 +0100

    Third time lucky

--- test/test_cli.py
+++ test/test_cli.py
@@ -33,7 +33,7 @@ def test_cli_parse():
 
 
 def test_args_passed_to_start(monkeypatch):
-    monkeypatch.setenv("VAULT_GITHUB_AUTH_TOKEN", "abcde")
+    monkeypatch.setenv("VAULT_AUTH_GITHUB_TOKEN", "abcde")
     with mock.patch('src.beebop_cli.beebop_start') as f:
         beebop_cli.main(["start", "prod"])
 
